53f1f506df040a377e8cbfb55c6d036f4f245b7d
ventusaguirre <ventusaguirre@my.whccd.edu>
2026-02-13 13:48:41 -0800

    added code for WelcomeMessage method, began working on UserInfo method

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -2,9 +2,25 @@
 {
     internal class Program
     {
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Coding Assignment #5!");
+        }
+
+
+        static void UserInfo(string name, int number)
+        {
+            Console.WriteLine("Enter your name: ");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = Console.ReadLine();
+            int number = Console.ReadLine();
+
+            WelcomeMessage();
+            UserInfo();
+
         }
     }
 }
