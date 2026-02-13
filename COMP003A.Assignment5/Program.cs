0ae14b90bf98d32cf1ca7d4462867b2b129b35e4
ventusaguirre <ventusaguirre@my.whccd.edu>
2026-02-13 15:47:01 -0800

    added switch function to determine behavior based on user input; changed from do-while to while

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -4,13 +4,12 @@
     {
         static void WelcomeMessage()
         {
-            Console.WriteLine("Welcome to Coding Assignment #5!\n");
+            Console.WriteLine("Welcome to Coding Assignment #5!");
         }
-
 
         static void UserInfo(string name, int a)
         {
-            Console.WriteLine($"\nYour name is {name} and your favorite number is {a}.\n");
+            Console.WriteLine($"\n{name} is a weird name, but {a} is a good number.\n");
         }
 
         static int CalculateValue(int a)
@@ -20,28 +19,57 @@
 
         static void Main(string[] args)
         {
+            int counter = 0;
 
             WelcomeMessage();
 
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
+            while (counter < 5) 
+            {
+                Console.Write("\n\nEnter your name: ");
+                string name = Console.ReadLine();
 
-            Console.Write("Enter your favorite number: ");
-            string number = Console.ReadLine();
-            int a = int.Parse(number);
+                Console.Write("Enter your favorite number: ");
+                int a = int.Parse(Console.ReadLine());
 
-            UserInfo(name, a);
+                UserInfo(name, a);
 
-            int b = CalculateValue(a);
+                int b = CalculateValue(a);
 
-            Console.WriteLine($"Calculated value: {b}");
+                Console.WriteLine($"Calculated value: {b}");
 
-            if (b < 50) {  Console.WriteLine("The result is less than 50."); }
-            else { Console.WriteLine("The result is greater than or equal to 50."); }
+                if (b < 50)
+                {
+                    Console.WriteLine("The result is less than 50.\n");
+                }
+                else
+                {
+                    Console.WriteLine("The result is greater than or equal to 50.\n");
+                }
 
+                Console.Write("Would you like to run the program again? (y/n): ");
+                string userChoice = Console.ReadLine();
 
+                switch (userChoice)
+                {
+                    case "y":
+                        counter++;
+                        if (counter >= 5)
+                        {
+                            Console.WriteLine("\nThat's too many times! I'm done now, goodbye!");
+                        }
+                        break;
 
+                    case "n":
+                        Console.WriteLine("\nThat was fun, goodbye!");
+                        Environment.Exit(0);
+                        break;
 
+                    default:
+                        Console.WriteLine("\nWhat's that supposed to mean? I'm done now. Goodbye!");
+                        Environment.Exit(0);
+                        break;
+                }
+            }
         }
     }
 }
